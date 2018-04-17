205717956916b42700ba5cf613c4a0f82f582aa7
ayoubalalawi <alwia2m@gmail.com>
2018-04-18 00:07:56 +0400

    Added my project

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: true,
+
+  dialogflow: {
+    angularBot: '9bc888f1d9fc4f228db83e7f3198d19b',
+    production: true
+  }
+};
