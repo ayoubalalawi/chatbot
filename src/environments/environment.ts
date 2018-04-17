7bf14893ad8bef520ad3b765554abed3c0494535
ayoubalalawi <alwia2m@gmail.com>
2018-04-18 00:20:07 +0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   production: true,
 
   dialogflow: {
-    angularBot: '9bc888f1d9fc4f228db83e7f3198d19b',
+    angularBot: 'Client_Key_Here',
     production: true
   }
 };
